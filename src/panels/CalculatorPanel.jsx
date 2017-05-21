73820d58bb2bee2651a1e3c96ab66be8ac49d91e
Shiva YB <shiva_yb@apple.com>
2017-05-21 22:19:12 +0530

    fixing some bugs

--- src/panels/CalculatorPanel.jsx
+++ src/panels/CalculatorPanel.jsx
@@ -1,3 +1,4 @@
+'use strict';
 import React, { Component } from 'react';
 import HistoryPanel from './HistoryPanel';
 import Results from './Results';
@@ -7,137 +8,303 @@ import Functions from './Functions';
 export default class CalculatorPanel extends Component {
 	constructor() {
 		super();
-		this._inProgress = false;
+		this._inProgress = true;
 	}
 	static defaultProps = {
 		OPERATIONS: ['/', 'X', '-', '+', '=']
 	};
 
 	state = {
+		operation: "",
 		actionArr: [],
 		result: "",
 		history: [],
 		reset: false
 	};
 
+	countOfOperations(arr) {
+		let { OPERATIONS } = this.props;
+		return arr.reduce((count, value) => {
+			if (OPERATIONS.includes(value)) {
+				return count + 1;
+			}
+			return count;
+		}, 0);
+	}
+
 	//use it recursively in case of "="
 	updateActionArr(value, recursive = false) {
 		let { OPERATIONS } = this.props;
-		let {actionArr, history} = this.state;
+		let {actionArr, history, operation} = this.state;
 
 		let operatorPresent = actionArr.find(action => OPERATIONS.includes(action));
-		
-		if (!this._inProgress && !operatorPresent && !OPERATIONS.includes(value)) {  //it's data, not a function so need to concatenate
-			const concatenated = actionArr.join('') + value
+		let tempArr = actionArr.slice(0);
+		if (this._inProgress) {  //it's data, not a function so need to concatenate
+			let concatenated;
+			if (operation === "") {
+				concatenated = actionArr.join('');
+			} else {
+				concatenated = operation;
+			}
+			concatenated = OPERATIONS.includes(value) ? `${concatenated} ${value} `: `${concatenated}${value}`;
+			if (value === "=") {
+				concatenated = "";
+			}
 			this.setState({
 				reset: false,
-				result: actionArr.join('') + value,
-				actionArr: [concatenated]
+				operation: concatenated,
+				result: ""
 			});
-			return;
+			// return;
+		} else {
+			tempArr = [];
+			this._inProgress = true;
+			this.setState({
+				reset: false,
+				operation: value,
+				result: ""
+			});
 		}
-		let tempArr = actionArr.slice();
 		if (!recursive) {  // "=" not pressed, so continue to next operation
-			tempArr.push(value);
+			if (!OPERATIONS.includes(value)) {
+				let prevValue = tempArr[tempArr.length - 1];
+				if (!!prevValue && !OPERATIONS.includes(prevValue)) {
+					tempArr.pop();
+					tempArr.push(prevValue + value); 
+				} else {
+					tempArr.push(value);
+				} 
+			} else {
+				tempArr.push(value);
+			}
 		} else {
 			value = tempArr[1];
 		}
-		if (tempArr.length <= 2 && value !== "=") { // not enough operands to perform any operation
-			this.setState({
-				reset: false,
-				result: value,
-				actionArr: tempArr
-			});
-			return "";
-		} else {  // we now have enough operands to perform an operation
-			let operands = [];
-			let result = "";
-			let nonFunctions = tempArr.filter(item => !OPERATIONS.includes(item));
-			if (!nonFunctions.length) {
-				this.setState({
-					reset: false,
-					result: value,
-					actionArr: []
-				});
-				return "";
-			} else {
-				switch (value) {
-					case "/": 
-						//get operands
-						operands = tempArr.filter(item => item !== value);
-						if (operands.length > 2) {
-							let partialResult = this.updateActionArr("=", true);
-							result = [partialResult, "/"];
-						} else {
-							result = this.divide(...operands, recursive);
-						}
-						this.updateState(result, recursive);
-						return result;
-					case "*":
-					case "X": 
-						operands = tempArr.filter(item => item !== value);
-						if (operands.length > 2) {
-							let partialResult = this.updateActionArr("=", true);
-							result = [partialResult, "X"];
-						} else {
-							result = this.multiply(...operands, recursive);
-						}
-						this.updateState(result, recursive);
-						return result;;
-					case "-": 
-						operands = tempArr.filter(item => item !== value);
-						if (operands.length > 2) {
-							let partialResult = this.updateActionArr("=", true);
-							result = [partialResult, "-"];
-						} else {
-							result = this.subtract(...operands, recursive);
-						}
-						this.updateState(result, recursive);
-						return result;
-					case "+": 
-						operands = tempArr.filter(item => item !== value);
-						if (operands.length > 2) {
-							let partialResult = this.updateActionArr("=", true);
-							result = [partialResult, "+"];
-						} else {
-							result = this.add(...operands, recursive);
-						}
+		this.setState({
+			reset: false,
+			result: "",
+			actionArr: tempArr
+		});
+		let result = "";
+		// if (!recursive && this.countOfOperations(tempArr) < 2 && value !== "=") { // not enough operands to perform any operation
+		// 	this.setState({
+		// 		reset: false,
+		// 		result: "",
+		// 		actionArr: tempArr
+		// 	});
+		// 	return "";
+		// } else {  // we now have enough operands to perform an operation
+			// if (!nonFunctions.length) {
+			// 	this.setState({
+			// 		reset: false,
+			// 		result: value,
+			// 		actionArr: []
+			// 	});
+			// 	return "";
+			// } else {
+			// 	switch (value) {
+			// 		case "/": 
+			// 			//get operands
+			// 			operands = tempArr.filter(item => item !== value);
+			// 			if (operands.length > 2) {
+			// 				let partialResult = this.updateActionArr("=", true);
+			// 				result = [partialResult, "/"];
+			// 			} else {
+			// 				result = this.divide(...operands, recursive);
+			// 			}
+			// 			this.updateState(result, recursive);
+			// 			break;
+			// 		case "*":
+			// 		case "X": 
+			// 			operands = tempArr.filter(item => item !== value);
+			// 			let length = tempArr.length;
+			// 			if (length > 2) {
+			// 				tempArr.slice(-3);
+			// 			}
+			// 			let partialResultPresent = false;
+			// 			if (tempArr[tempArr.length - 1] === value) {
+			// 				let partialResult = this.updateActionArr("X", true);
+			// 				result = [partialResult];
+			// 				partialResultPresent = true;
+			// 			} else {
+			// 				result = this.multiply(...operands, recursive);
+			// 			}
+			// 			this.updateState(result, recursive, partialResultPresent);
+			// 			break;
+			// 		case "-": 
+			// 			operands = tempArr.filter(item => item !== value);
+			// 			if (operands.length > 2) {
+			// 				let partialResult = this.updateActionArr("=", true);
+			// 				result = [partialResult, "-"];
+			// 			} else {
+			// 				result = this.subtract(...operands, recursive);
+			// 			}
+			// 			this.updateState(result, recursive);
+			// 			break;
+			// 		case "+": 
+			// 			operands = tempArr.filter(item => item !== value);
+			// 			if (operands.length > 2) {
+			// 				let partialResult = this.updateActionArr("=", true);
+			// 				result = [partialResult, "+"];
+			// 			} else {
+			// 				result = this.add(...operands, recursive);
+			// 			}
 						
-						this.updateState(result, recursive);
-						return result;
-					case "=": //show result and save it for History
-						this.updateActionArr("=", true);
-						return result;;
-					default:
-						this.setState({
-							reset: false, 
-							result: value,
-							actionArr: tempArr
-						});
+			// 			this.updateState(result, recursive);
+			// 			break;
+			// 		case "=": //show result and save it for History
+			// 			result = this.updateActionArr("=", true);
+			// 			break;
+			// 		default:
+			// 			this.setState({
+			// 				reset: false, 
+			// 				result: value,
+			// 				actionArr: tempArr
+			// 			});
+			// 			result = "";
+			// 	}
+			// }
+				if (tempArr.length > 2) {
+					result = this.performOperations(value, tempArr.slice(0));
 				}
-			}
-			
-		}
+				this.setState({
+					actionArr: tempArr
+				});
+				return result;
+			// }
+		// }
 	}
 
-	updateState(result, saveResult) {
-		let { history, actionArr } = this.state;
+	performOperations(operation, tempArr, recursive = false) {
+		let result = "";
+		let operands = [];
+		let length = 0;
+		let partialResultPresent = false;
+		let partialResult = "";
+		switch (operation) {
+			case "/": 
+				//get operands
+				operands = tempArr.filter(item => item !== operation);
+				length = tempArr.length;
+				let partialResultPresent = false;
+				if (length > 2) {
+					if (tempArr[tempArr.length - 1] === operation) {
+						tempArr.splice(-1);
+						tempArr.push("=");
+						partialResult = this.performOperations("=", tempArr, true);
+						result = [partialResult];
+						partialResultPresent = true;
+					} else {
+						result = this.divide(...operands, recursive);
+					}
+				}
+				this.updateState(result, false, partialResultPresent);
+				break;
+			case "X": 
+				operands = tempArr.filter(item => item !== operation);
+				length = tempArr.length;
+				partialResultPresent = false;
+				if (length > 2) {
+					if (tempArr[tempArr.length - 1] === operation) {
+						tempArr.splice(-1);
+						tempArr.push("=");
+						partialResult = this.performOperations("=", tempArr, true);
+						result = [partialResult];
+						partialResultPresent = true;
+					} else {
+						result = this.multiply(...operands, recursive);
+					}
+				}
+				this.updateState(result, false, partialResultPresent);
+				break;
+			case "-": 
+				operands = tempArr.filter(item => item !== operation);
+				length = tempArr.length;
+				partialResultPresent = false;
+				if (length > 2) {
+					if (tempArr[tempArr.length - 1] === operation) {
+						tempArr.splice(-1);
+						tempArr.push("=");
+						partialResult = this.performOperations("=", tempArr, true);
+						result = [partialResult];
+						partialResultPresent = true;
+					} else {
+						result = this.subtract(...operands, recursive);
+					}
+				}
+				this.updateState(result, false, partialResultPresent);
+				break;
+			case "+": 
+				operands = tempArr.filter(item => item !== operation);
+				length = tempArr.length;
+				partialResultPresent = false;
+				if (length > 2) {
+					if (tempArr[tempArr.length - 1] === operation) {
+						tempArr.splice(-1);
+						tempArr.push("=");
+						partialResult = this.performOperations("=", tempArr, true);
+						result = [partialResult];
+						partialResultPresent = true;
+					} else {
+						result = this.add(...operands, recursive);
+					}
+				}
+				this.updateState(result, false, partialResultPresent);
+				break;
+			case "=": //show result and save it for History
+				tempArr.splice(-1);
+				do {
+					console.log(tempArr);
+					let lastOp = tempArr.splice(-3);
+					operation = lastOp[1];
+					result = this.performOperations(operation, lastOp, true);
+					// tempArr = tempArr.slice(-2);
+					tempArr.push(result);
+				} while (tempArr.length >= 2)
+				this.updateState(result, !recursive);
+				break;
+			default:
+				this.setState({
+					reset: false, 
+					actionArr: tempArr
+				});
+				result = "";
+		}
+		return result;
+	}
+
+	updateState(result, saveResult, partialResultPresent = false) {
+		let { history, actionArr, operation } = this.state;
 		let tempHistArr = history.slice();
-		if (saveResult) {
-			tempHistArr.unshift(actionArr.join(' '));
-			let concatenated = `${tempHistArr[0]} = ${result}`;
-			tempHistArr[0] = concatenated;
-			this._inProgress = false;
-		} else {
-			this._inProgress = true;
+		result = result instanceof Array ? result: [result];
+		if (partialResultPresent) {
+			this.setState({
+				reset: false, 
+				result: result[0],
+				history: tempHistArr
+			});
+			return;
+		} else if (saveResult) {
+			let concatenated = `${operation} = ${result}`;
+			tempHistArr.unshift(concatenated);
 		}
 		result = result instanceof Array ? result: [result];
-		this.setState({
-		reset: false, 
-			result: result[0],
-			actionArr: result,
-			history: tempHistArr
-		});
+		if (saveResult) {
+			this.setState({
+				reset: false,
+				actionArr: [],
+				result: result[0],
+				history: tempHistArr
+			});
+			this._inProgress = false;
+		} else {
+			this.setState({
+				reset: false,
+				result: result[0]
+			});
+			this._inProgress = true;
+		}
+		
 	}
 
 	divide(a, b) {
@@ -173,6 +340,7 @@ export default class CalculatorPanel extends Component {
 	reset() {
 		this.setState({
 			result: "",
+			operation: "",
 			actionArr: [],
 			history: [],
 			reset: true
@@ -180,7 +348,8 @@ export default class CalculatorPanel extends Component {
 	}
 
 	render() {
-		let { result, actionArr, history, reset } = this.state;
+		let { result, actionArr, history, reset, operation } = this.state;
+		let className = this._inProgress ? "" :  "final";
 		return (
 			<div className="main-view">
 				<div className="history-view">
@@ -188,7 +357,7 @@ export default class CalculatorPanel extends Component {
 					/>
 				</div>
 				<div className="calc-view">
-					<Results actionArr= { actionArr } result= { result }/>
+					<Results actionArr= { actionArr } operation={operation} result= { result } className={className}/>
 					<div className="flex">
 						<Keyboard 
 							updateActionArr={ this.updateActionArr.bind(this) }
--- src/panels/Results.jsx
+++ src/panels/Results.jsx
@@ -2,12 +2,20 @@ import React, { Component } from 'react';
 
 export default class Keyboard extends Component {
 	static defaultProps = {
-		result: ""
+		result: "",
+		operation: ""
 	};
 
 	render () {
+		let classNames = ["output"];
+		let { className } = this.props;
+		classNames.push(className);
 		return (
-			<div className="result">{ this.props.result }
+			<div className="result">
+				<div className="operation">{ this.props.operation }
+				</div>
+				<div className={classNames.join(" ")}>{ this.props.result }
+				</div>
 			</div>
 		);
 	}
